36ffd588281d373b15ebfe575afc8d8e71ce12ea
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-05-15 15:33:50 -0500

    added fuctionality to edit button

--- src/main/java/com/codeup/springblogapp/controllers/PostController.java
+++ src/main/java/com/codeup/springblogapp/controllers/PostController.java
@@ -1,6 +1,5 @@
 package com.codeup.springblogapp.controllers;
 
-
 import com.codeup.springblogapp.model.Post;
 import com.codeup.springblogapp.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
@@ -17,7 +16,7 @@ public class PostController {
     }
 
 
-
+    //******************** method for view all the blogs on the index page*************************
     @GetMapping("/posts")
     public String getPosts(Model model) {
         model.addAttribute("posts", postRepo.findAll());
@@ -25,6 +24,7 @@ public class PostController {
     }
 
 
+//******************** method for grabbing individual post by id and view it on the post/{id} page**************
     @GetMapping("/posts/{id}")
     public String getPostById(@PathVariable long id, Model model) {
         Post aPost = postRepo.getOne(id);
@@ -32,17 +32,62 @@ public class PostController {
         return "post/show";
     }
 
+
+//******************** method for taking user to create post page *******************************
     @GetMapping("/posts/create")
     public String createPost() {
         return "post/create";
     }
 
+
+
+//******************* method for creating a new post and rerouting user to post/{id} page *******************
     @PostMapping("/posts/create")
     public String submitCreatePost(@RequestParam(name = "blog-title") String title, @RequestParam(name = "blog-body") String body) {
         Post newPost = new Post();
         newPost.setTitle(title);
         newPost.setBody(body);
         newPost = this.postRepo.save(newPost);
+        return "redirect:/posts";
+    }
+
+
+//****************** method for getting edit page **************************
+    @GetMapping("/posts/{id}/edit")
+    public String getEditPostForm(@PathVariable long id, Model model){
+        Post aPost = postRepo.getOne(id);
+        model.addAttribute("post", aPost);
+        return "post/edit";
+    }
+
+
+
+//****************** method for editing post and rerouting user to post page ******************
+    @PostMapping("/posts/{id}/edit")
+    public String savePostEdit(@ModelAttribute Post post, @RequestParam(name = "blog-title") String title, @RequestParam(name = "blog-body") String body, Model model){
+        Post editPost = postRepo.getOne(post.getId());
+        editPost.setTitle(title);
+        editPost.setBody(body);
+        postRepo.save(editPost);
+//        model.addAttribute("post", editPost);
+        return "redirect:/posts/" + post.getId();
+    }
+
+
+
+    @GetMapping("/posts/{id}/delete")
+    public String getDeletePostForm(@PathVariable long id, Model model){
+        Post singlePost = postRepo.getOne(id);
+        model.addAttribute("post", singlePost);
+        return "posts/delete";
+    }
+
+
+
+    @PostMapping("/posts/{id}/delete")
+    public String deletePost(@PathVariable long id){
+        Post singlePost = postRepo.getOne(id);
+        postRepo.delete(singlePost);
         return "redirect:/posts";
     }
 
