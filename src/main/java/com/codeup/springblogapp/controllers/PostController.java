79147bd8fcae1d474944e9c31c363619eb13570b
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-05-18 17:17:27 -0500

    completed form control exercise

--- src/main/java/com/codeup/springblogapp/controllers/PostController.java
+++ src/main/java/com/codeup/springblogapp/controllers/PostController.java
@@ -32,17 +32,20 @@ public class PostController {
 //******************** method for grabbing individual post by id and view it on the post/{id} page**************
     @GetMapping("/posts/{id}")
     public String getPostById(@PathVariable long id, Model model) {
-        Post aPost = postRepo.getOne(id);
-        model.addAttribute("posts", aPost);
-        return "post/show";
+        Post post = postRepo.getOne(id);
+        model.addAttribute("posts", post);
+        return "/post/show";
     }
 
 
 //******************** method for taking user to create post page *******************************
     @GetMapping("/posts/create")
     public String createPost(Model model) {
-        model.addAttribute("post", new Post());
-        return "post/create";
+        User user = userRepo.getOne(1L);
+        Post post = new Post();
+        post.setUser(user);
+        model.addAttribute("post", post);
+        return "/post/create";
     }
 
 
@@ -50,11 +53,6 @@ public class PostController {
 //******************* method for creating a new post and rerouting user to post/{id} page *******************
     @PostMapping("/posts/create")
     public String submitCreatePost(@ModelAttribute Post post) {
-        User user = userRepo.getOne(1L);
-//        Post newPost = new Post();
-//        newPost.setTitle(title);
-//        newPost.setBody(body);
-//        newPost.setUser(user);
         postRepo.save(post);
         return "redirect:/posts";
     }
@@ -63,8 +61,8 @@ public class PostController {
 //****************** method for getting edit page **************************
     @GetMapping("/posts/{id}/edit")
     public String getEditPostForm(@PathVariable long id, Model model){
-        Post aPost = postRepo.getOne(id);
-        model.addAttribute("post", aPost);
+        Post post = postRepo.getOne(id);
+        model.addAttribute("post", post);
         return "post/edit";
     }
 
@@ -72,14 +70,19 @@ public class PostController {
 
 //****************** method for editing post and rerouting user to post page ******************
     @PostMapping("/posts/{id}/edit")
-    public String savePostEdit(@ModelAttribute Post post, @RequestParam(name = "blog-title") String title, @RequestParam(name = "blog-body") String body, Model model){
-        Post editPost = postRepo.getOne(post.getId());
-        editPost.setTitle(title);
-        editPost.setBody(body);
-        postRepo.save(editPost);
-//        model.addAttribute("post", editPost);
+    public String savePostEdit(@ModelAttribute Post post){
+        postRepo.save(post);
         return "redirect:/posts/" + post.getId();
     }
+
+//    @GetMapping("/posts/editcreate")
+//    public String editCreatePost(Model model) {
+//        User user = userRepo.getOne(1L);
+//        Post post = new Post();
+//        post.setUser(user);
+//        model.addAttribute("post", post);
+//        return "/post/edit";
+//    }
 
 
 //********************* method for taking user to delete post page **********************
